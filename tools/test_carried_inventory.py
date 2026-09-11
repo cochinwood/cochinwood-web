aabeb5fb69f010e883ea23f66fa0d760568a8d12
Edwin David <edwin.david@cochinwood.in>
2026-09-11 14:31:53 +0530

    Optimize 15 large species botanical images saving 2.03 MB and refresh manifests

--- tools/test_carried_inventory.py
+++ tools/test_carried_inventory.py
@@ -10,6 +10,9 @@ class CarriedInventoryTests(unittest.TestCase):
             blobs = build._live_tree(path)
             self.assertIsNotNone(blobs, path)
             expected += len(blobs)
+        asset_blobs = build._live_tree('assets')
+        if asset_blobs:
+            expected += sum(1 for rel in asset_blobs if build.LIVE_HASHED_ASSET_RE.fullmatch(rel))
         self.assertEqual(build.carried_live_count(), expected)
 
 
